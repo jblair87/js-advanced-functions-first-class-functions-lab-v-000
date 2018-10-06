4d20f6df1ddc371a15d93d801651f718fafdbb4e
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-06 23:03:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const returnLastTwoDrivers = function (drivers) {
 const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers
   ];
   
-const createFareMultiplier = function(){};
+const createFareMultiplier = function(multiplyValue){};
 
 const fareDoubler = createFareMultiplier(2);
 

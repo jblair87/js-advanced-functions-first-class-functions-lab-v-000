8e1eef5db20a83aee3f5ca3007ede898881b6eca
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-06 22:51:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+ 
+};
+
+const returnLastTwoDrivers = function (drivers) {
+ 
+};
+
+const selectingDrivers = [
+  function () { console.log('Functions'); },
+  function () { console.log('are'); }
+  ];
